3c7dde7e35ab8877e49e466eba0aaeec26475ffb
Administrator <Administrator@KANINI-LTP-536>
2021-12-03 20:45:16 +0530

    submission of 2.2. Task1

---
+++ app/src/main/java/com/example/textinputviewapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.textinputviewapp
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+
+    var clickCountMap = mutableMapOf<String,Int>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clickCountDisplayView = findViewById<TextView>(R.id.clickCountView)
+
+
+        val clickMeButton = findViewById<TextView>(R.id.clickButton)
+
+        val userNameTextInputView = findViewById<TextInputLayout>(R.id.Nametext)
+        clickMeButton.setOnClickListener {
+            val userName = userNameTextInputView.editText?.text?.toString()
+            val maskedUserName =
+                if (userName.isNullOrEmpty()) "Somebody"
+                else userName
+
+          val  oldClickCount = clickCountMap[maskedUserName] ?:0
+            val newClickCount=oldClickCount + 1
+            clickCountMap[maskedUserName]=newClickCount
+
+            clickCountDisplayView.text = "$maskedUserName clicked $newClickCount  times"
+
+        }
+
+
+
+        }
+
+}
+
+
